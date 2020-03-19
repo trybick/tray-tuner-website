a46b749bfd86781a28a3528bf8bfc564658f5344
Tim Rybicki <t.rybicki@gmail.com>
2020-03-18 20:05:32 -0400

    Create a basic page for documentation

--- src/components/cta.js
+++ src/components/cta.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "gatsby";
 import Button from "./button";
 import SectionHeader from "./section-header";
 
 const CallToAction = () => (
   <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
     <SectionHeader title="Built with Electron" description="Supports macOS, Windows, and Linux" />
-    <Button>Documentation</Button>
+    <Link to="/docs/">
+      {" "}
+      <Button>Documentation</Button>
+    </Link>
   </div>
 );
 
