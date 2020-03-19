c58086be8d1c70345b707809d42d503973e58b33
Tim Rybicki <t.rybicki@gmail.com>
2020-03-19 19:50:51 -0400

    Add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
+import Helmet from "react-helmet";
 
 import Footer from "./footer";
 import "../styles/default.css";
+import favicon from "../images/favicon.ico";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,6 +27,9 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <Helmet>
+          <link rel="icon" href={favicon} />
+        </Helmet>
         <div
           style={{
             padding: "0 1rem",
