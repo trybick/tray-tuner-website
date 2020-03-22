ce8946b70b153d98ff9ca641318f5527490f3000
Tim Rybicki <t.rybicki@gmail.com>
2020-03-22 19:44:14 -0400

    Update download link with readme blob

--- src/components/cta.js
+++ src/components/cta.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './button';
 import SectionHeader from './section-header';
 
-const documentationLink = 'https://github.com/trybick/tray-tuner#tray-tuner';
+const documentationLink = 'https://github.com/trybick/tray-tuner/blob/master/README.md#usage';
 
 const CallToAction = () => (
   <div style={{ padding: '4rem 1rem', textAlign: 'center' }}>
