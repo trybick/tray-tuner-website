39800431379f9a41da07f687a16f5240c20a98f6
Tim Rybicki <t.rybicki@gmail.com>
2020-03-23 19:45:24 -0400

    WIP error on importing markdown file

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useEffect, useState } from 'react';
+import marked from 'marked';
+// import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-// Not in use. Saving as an example of a second page.
+const fetch = require('node-fetch');
 
-const DocsPage = () => (
-  <Layout>
-    <SEO />
-    {/* <Link to="/">Go back to the homepage</Link> */}
+const DocsPage = () => {
+  const [markdownToRender, setMarkdownToRender] = useState();
 
-    <div className="docs-wrapper">
-      <div className="markdown-body">Test Markdown</div>
-    </div>
-  </Layout>
-);
+  useEffect(() => {
+    const docsMarkdownPath = require('../markdown/docs.md');
+
+    // fetch(docsMarkdownPath)
+    //   .then(response => {
+    //     console.log('response:', response);
+    //     return response.text();
+    //   })
+    //   .then(text => {
+    //     console.log('text:', text);
+    //     setMarkdownToRender(marked(text));
+    //   });
+  }, []);
+
+  return (
+    <Layout>
+      <SEO />
+      {/* <Link to="/">Go back to the homepage</Link> */}
+
+      <div className="docs-wrapper">
+        <div className="markdown-body">Placeholder</div>
+      </div>
+    </Layout>
+  );
+};
 
 export default DocsPage;
